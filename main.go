eda722760fa15669d140a705e7dfae4ea4f067c7
teyik0 <teyik0.fr@icloud.com>
2023-09-28 13:58:32 +0200

    cloning all the repos working

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/joho/godotenv"
 )
@@ -125,23 +127,54 @@ type Owner struct {
 }
 
 func main() {
-	jsonData, err := fetchAllRepoFromUser("MTthoas")
+	jsonData, err := fetchAllRepoFromUser("Teyik0")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("test", err)
 	}
-	fmt.Println(jsonData)
 
 	// Unmarshal JSON data
 	var repos []Repo
 	if err := json.Unmarshal([]byte(jsonData), &repos); err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return
+		log.Fatal("Error decoding JSON:", err)
 	}
 
-	// Print out the results
+	// Create a CSV file
+	createCSVFile(repos)
+
+	// Clone all repos
+	cloneRepo(repos)
+
+	// Git pull all repos
+
+}
+
+func cloneRepo(repos []Repo) {
+	//Run the git clone command
+	folderPath := "repos"
+
+	_, err1 := os.Stat(folderPath)
+	if err1 != nil {
+		fmt.Println("Creating folder repos...")
+		os.Mkdir(folderPath, os.ModePerm)
+	}
+	err2 := os.Chmod(folderPath, 0777)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
 	for _, repo := range repos {
-		fmt.Printf("Repository Name: %s\n", repo.FullName)
-		fmt.Printf("Owner Login: %s\n", repo.Owner.Login)
+		fmt.Printf("Cloning repository : %s ....\n", repo.FullName)
+		newPath := folderPath + "/" + repo.Name
+		_, err := os.Stat(newPath)
+		if err != nil {
+			os.Mkdir(newPath, os.ModePerm)
+			cmd := exec.Command("git", "clone", repo.CloneURL, newPath)
+			if err := cmd.Run(); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Printf("Directory already exists : %s", newPath)
+		}
 	}
 }
 
@@ -150,7 +183,7 @@ func fetchAllRepoFromUser(username string) (string, error) {
 
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err)
 	}
 	tokenUrl, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok {
@@ -168,13 +201,118 @@ func fetchAllRepoFromUser(username string) (string, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("Error on response.\n[ERROR] -", err)
+		log.Fatal("Error on response.\n[ERROR] -", err)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("test1", err)
 	}
 
 	return string(body), err
+}
+
+func createCSVFile(repos []Repo) error {
+	// Create a CSV file
+	file, err := os.Create("repos.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Create a CSV writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write header
+	header := []string{"ID", "NodeID", "Name", "FullName", "Private", "Owner", "HTML_URL", "Description", "Fork", "Url", "ForksURL", "KeysURL", "CollaboratorsURL", "TeamsURL", "HooksURL", "IssueEventsUrl", "EventsURL", "AssigneesURL", "BranchesURL", "TagsURL", "BlobsURL", "GitTagsURL", "GitRefsURL", "TreesURL", "StatusesURL", "LanguagesURL", "StargazersURL", "ContributorsURL", "SubscribersURL", "SubscriptionURL", "CommitsURL", "GitCommitsURL", "CommentsURL", "IssueCommentURL", "ContentsURL", "CompareURL", "MergesURL", "ArchiveURL", "DownloadsURL", "IssuesURL", "PullsURL", "MilestonesURL", "NotificationsURL", "LabelsURL", "ReleasesURL", "DeploymentsURL", "CreatedAt", "UpdatedAt", "PushedAt", "GitURL", "SshURL", "CloneURL", "SvnURL", "Homepage", "Size", "StargazersCount", "WatchersCount", "Language", "HasIssues", "HasProjects", "HasDownloads", "HasWiki", "HasPages", "ForksCount", "MirrorURL", "Archived", "Disabled", "OpenIssuesCount", "License", "AllowForking", "IsTemplate", "WebCommitSignoffRequired", "Permission"}
+	if err := writer.Write(header); err != nil {
+		panic(err)
+	}
+
+	// Write data
+	for _, repo := range repos {
+		record := []string{
+			// Convert each field to a string
+			fmt.Sprint(repo.ID),
+			repo.NodeID,
+			repo.Name,
+			repo.FullName,
+			fmt.Sprint(repo.Private),
+			fmt.Sprint(repo.Owner),
+			repo.HTML_URL,
+			repo.Description,
+			fmt.Sprint(repo.Fork),
+			repo.Url,
+			repo.ForksURL,
+			repo.KeysURL,
+			repo.CollaboratorsURL,
+			repo.TeamsURL,
+			repo.HooksURL,
+			repo.IssueEventsUrl,
+			repo.EventsURL,
+			repo.AssigneesURL,
+			repo.BranchesURL,
+			repo.TagsURL,
+			repo.BlobsURL,
+			repo.GitTagsURL,
+			repo.GitRefsURL,
+			repo.TreesURL,
+			repo.StatusesURL,
+			repo.LanguagesURL,
+			repo.StargazersURL,
+			repo.ContributorsURL,
+			repo.SubscribersURL,
+			repo.SubscriptionURL,
+			repo.CommitsURL,
+			repo.GitCommitsURL,
+			repo.CommentsURL,
+			repo.IssueCommentURL,
+			repo.ContentsURL,
+			repo.CompareURL,
+			repo.MergesURL,
+			repo.ArchiveURL,
+			repo.DownloadsURL,
+			repo.IssuesURL,
+			repo.PullsURL,
+			repo.MilestonesURL,
+			repo.NotificationsURL,
+			repo.LabelsURL,
+			repo.ReleasesURL,
+			repo.DeploymentsURL,
+			repo.CreatedAt,
+			repo.UpdatedAt,
+			repo.PushedAt,
+			repo.GitURL,
+			repo.SshURL,
+			repo.CloneURL,
+			repo.SvnURL,
+			repo.Homepage,
+			fmt.Sprint(repo.Size),
+			fmt.Sprint(repo.StargazersCount),
+			fmt.Sprint(repo.WatchersCount),
+			repo.Language,
+			fmt.Sprint(repo.HasIssues),
+			fmt.Sprint(repo.HasProjects),
+			fmt.Sprint(repo.HasDownloads),
+			fmt.Sprint(repo.HasWiki),
+			fmt.Sprint(repo.HasPages),
+			fmt.Sprint(repo.ForksCount),
+			repo.MirrorURL,
+			fmt.Sprint(repo.Archived),
+			fmt.Sprint(repo.Disabled),
+			fmt.Sprint(repo.OpenIssuesCount),
+			repo.License.Name,
+			fmt.Sprint(repo.AllowForking),
+			fmt.Sprint(repo.IsTemplate),
+			fmt.Sprint(repo.WebCommitSignoffRequired),
+			fmt.Sprint(repo.Permission),
+			// Add more fields as needed
+		}
+		if err := writer.Write(record); err != nil {
+			panic(err)
+		}
+	}
+
+	return nil
 }
